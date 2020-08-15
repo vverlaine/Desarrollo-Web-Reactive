56b7735eb35967d4a54683fc3f862d47280c1233
verlaine.victor@gmail.com <Manantial68!>
2020-08-15 18:03:23 +0200

    Comit Prueba 3

--- src/main/java/com/example/DesarrolloWebReactive/Configuration/CorsConfiguration.java
+++ src/main/java/com/example/DesarrolloWebReactive/Configuration/CorsConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-//
+
 @Configuration
 public class CorsConfiguration {
 
